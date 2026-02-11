8d87f9af8c3396f2664a9c61b4e0189303ccb790
moham <mohammadmirahmadi2@gmai.com>
2026-02-11 19:31:33 +0330

    user management implemented.

--- CarRentalSystem.cpp
+++ CarRentalSystem.cpp
@@ -1,17 +1,31 @@
 #include "CarRentalSystem.h"
 #include <iostream>
 
-CarRentalSystem::CarRentalSystem(){
-
+CarRentalSystem::CarRentalSystem() : idCounter(1), currentUser(nullptr){
+    signUp("admin","admin",STAFF);
 }
 void CarRentalSystem::signUp(string userName, string pass, Role role=CUSTOMER){
-
+    if(table.search(userName) != nullptr){
+        cout<<"userName exists, choose another."<<endl;
+        return;
+    }
+    User* newUser=new User(userName, pass, role);
+    userList.append(newUser);
+    table.insert(userName,newUser);
+    cout<<"Registeration was successful!"<<endl;
 }
 void CarRentalSystem::login(string userName, string pass){
-
+    User* user=table.search(userName);
+    if(user!=nullptr && user->passHash==hashString(pass)){
+        currentUser=user;
+        cout<<"Welcome "<<userName<<"!"<<endl;
+        return;
+    }
+    cout<<"login failed!\n";
 }
 void CarRentalSystem::logOut(){
-
+    cout<<currentUser->userName<<" logged out"<<endl;
+    currentUser=nullptr;
 }
 
 void CarRentalSystem::addCar(string company , string model, double price){
@@ -33,5 +47,5 @@ void CarRentalSystem::returnCar(string model){
 
 }
 void CarRentalSystem::availableCars(){
-    
+
 }
